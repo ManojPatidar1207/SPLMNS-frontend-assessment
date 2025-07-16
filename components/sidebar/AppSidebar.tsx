9edeffb33e580ebad48065d39b37f01eacb755b3
ManojPatidar1207 <manojpatidar1207@gmail.com>
2025-07-16 21:49:54 +0530

    Show active item in sidebar

--- components/sidebar/AppSidebar.tsx
+++ components/sidebar/AppSidebar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import {
   Sidebar,
@@ -14,12 +15,13 @@ import {
 } from "@/components/ui/sidebar";
 
 import { useIsMobile } from "@/hooks/use-mobile";
-
+import { cn } from "@/lib/utils";
 import { sidebarItems } from "@/lib/configs";
 
 const AppSidebar: React.FC = () => {
   const { setOpenMobile } = useSidebar();
   const isMobile = useIsMobile();
+  const pathname = usePathname();
 
   return (
     <Sidebar>
@@ -28,19 +30,28 @@ const AppSidebar: React.FC = () => {
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
-              {sidebarItems.map((item) => (
-                <SidebarMenuItem
-                  key={item.title}
-                  onClick={() => isMobile && setOpenMobile(false)}
-                >
-                  <SidebarMenuButton asChild>
-                    <Link href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {sidebarItems.map((item) => {
+                const isActive = pathname === item.url;
+
+                return (
+                  <SidebarMenuItem
+                    key={item.title}
+                    onClick={() => isMobile && setOpenMobile(false)}
+                  >
+                    <SidebarMenuButton
+                      asChild
+                      className={cn({
+                        "!bg-primary/20 text-primary": isActive,
+                      })}
+                    >
+                      <Link href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
